e12be8aa02c344b2a7f6d3d77749826f8cf93250
Carlos Alvarez Moreno <ceam1710@gmail.com>
2019-12-10 11:29:15 -0500

    Listening in Different Places & Ways

--- lib/providers/products_provider.dart
+++ lib/providers/products_provider.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:shop_app/models/product.dart';
+import 'package:shop_app/providers/product.dart';
 
 class Products with ChangeNotifier {
 
@@ -38,6 +38,10 @@ class Products with ChangeNotifier {
 
   List<Product> get items{
     return [..._items];
+  }
+
+  Product findById(String id){
+    return _items.firstWhere((prod)=> prod.id == id);
   }
 
   void addProduct(){
