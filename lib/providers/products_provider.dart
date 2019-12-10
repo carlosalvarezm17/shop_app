a2acc84d28014b9c658b968702579f7f24b0be64
Carlos Alvarez Moreno <ceam1710@gmail.com>
2019-12-10 13:23:16 -0500

    Local State vs App-wide state

--- lib/providers/products_provider.dart
+++ lib/providers/products_provider.dart
@@ -36,9 +36,28 @@ class Products with ChangeNotifier {
           'https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/Cast-Iron-Pan.jpg/1024px-Cast-Iron-Pan.jpg',
     ),];
 
+  // var _showFavoritesOnly = false;
+
   List<Product> get items{
+   
     return [..._items];
   }
+
+  List<Product> get favoriteItems{
+    
+    return _items.where((prodItem) => prodItem.isFavorite).toList();
+   
+  }
+
+  // void showFavoritesOnly(){
+  //   _showFavoritesOnly = true;
+  //   notifyListeners();
+  // }
+
+  // void showAll(){
+  //   _showFavoritesOnly = false;
+  //   notifyListeners();
+  // }
 
   Product findById(String id){
     return _items.firstWhere((prod)=> prod.id == id);
