71aa52ecc4a9187e21bd63b46f2927da472b9744
Carlos Alvarez Moreno <ceam1710@gmail.com>
2020-01-15 09:49:31 -0500

    Adding a "Manage Products" Page

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ import 'package:shop_app/screens/orders_screen.dart';
 import 'package:shop_app/screens/product_detail_screen.dart';
 import 'package:shop_app/screens/products_overview_screen.dart';
 import 'package:shop_app/providers/products_provider.dart';
+import 'package:shop_app/screens/user_products_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -38,6 +39,7 @@ class MyApp extends StatelessWidget {
           ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
           CartScreen.routeName: (ctx) => CartScreen(),
           OrdersScreen.routeName: (ctx) => OrdersScreen(),
+          UserProductsScreen.routeName: (ctx) => UserProductsScreen(),
         },
       ),
     );
