1b0c60f7da3ee2925c5f14378fcd08f3d8f7d77e
Carlos Alvarez Moreno <ceam1710@gmail.com>
2020-01-15 10:09:52 -0500

    Using Forms & Working with Form Inputs

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:shop_app/providers/orders.dart';
 import 'package:shop_app/screens/cart_screen.dart';
 import 'package:shop_app/screens/orders_screen.dart';
 import 'package:shop_app/screens/product_detail_screen.dart';
+import 'package:shop_app/screens/product_screen.dart';
 import 'package:shop_app/screens/products_overview_screen.dart';
 import 'package:shop_app/providers/products_provider.dart';
 import 'package:shop_app/screens/user_products_screen.dart';
@@ -40,6 +41,7 @@ class MyApp extends StatelessWidget {
           CartScreen.routeName: (ctx) => CartScreen(),
           OrdersScreen.routeName: (ctx) => OrdersScreen(),
           UserProductsScreen.routeName: (ctx) => UserProductsScreen(),
+          ProductScreen.routeName: (ctx) => ProductScreen(),
         },
       ),
     );
---
+++ lib/screens/product_screen.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class ProductScreen extends StatefulWidget {
+
+  static const routeName = '/product';
+
+  @override
+  _ProductScreenState createState() => _ProductScreenState();
+}
+
+class _ProductScreenState extends State<ProductScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Product'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          child: ListView(
+            children: <Widget>[
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Title'),
+                textInputAction: TextInputAction.next,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
