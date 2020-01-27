a35bbc1ccaad0fef3e0634470a152c62a209da1f
Carlos Alvarez Moreno <ceam1710@gmail.com>
2020-01-27 11:45:35 -0500

    Animations From Scratch (Completely Manually Controlled)

--- lib/screens/auth-screen.dart
+++ lib/screens/auth-screen.dart
@@ -93,7 +93,8 @@ class AuthCard extends StatefulWidget {
   _AuthCardState createState() => _AuthCardState();
 }
 
-class _AuthCardState extends State<AuthCard> {
+class _AuthCardState extends State<AuthCard>
+    with SingleTickerProviderStateMixin {
   final GlobalKey<FormState> _formKey = GlobalKey();
   AuthMode _authMode = AuthMode.Login;
   Map<String, String> _authData = {
@@ -102,20 +103,48 @@ class _AuthCardState extends State<AuthCard> {
   };
   var _isLoading = false;
   final _passwordController = TextEditingController();
+  AnimationController _controller;
+  Animation<Size> _heightAnimation;
 
-  void _showErrorDialog(String message){
-    showDialog(context: context, builder: (ctx)=> AlertDialog(
-      title: Text('An error occurred'),
-      content: Text(message),
-      actions: <Widget>[
-        FlatButton(
-          child: Text('Ok'),
-          onPressed: (){
-            Navigator.of(ctx).pop();
-          },
-        )
-      ],
-    ));
+  @override
+  void initState() {
+    _controller =
+        AnimationController(vsync: this, duration: Duration(milliseconds: 300));
+    _heightAnimation = Tween<Size>(
+      begin: Size(double.infinity, 260),
+      end: Size(double.infinity, 320),
+    ).animate(
+      CurvedAnimation(
+        parent: _controller,
+        curve: Curves.linear,
+      ),
+    );
+    _heightAnimation.addListener(() => setState((){}));
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    _controller.dispose();
+  }
+
+  void _showErrorDialog(String message) {
+    showDialog(
+        context: context,
+        builder: (ctx) => AlertDialog(
+              title: Text('An error occurred'),
+              content: Text(message),
+              actions: <Widget>[
+                FlatButton(
+                  child: Text('Ok'),
+                  onPressed: () {
+                    Navigator.of(ctx).pop();
+                  },
+                )
+              ],
+            ));
   }
 
   Future<void> _submit() async {
@@ -135,21 +164,21 @@ class _AuthCardState extends State<AuthCard> {
         await Provider.of<Auth>(context, listen: false)
             .signup(_authData['email'], _authData['password']);
       }
-    } on HttpException catch(err) {
+    } on HttpException catch (err) {
       var errorMessage = 'Authentication failed';
-      if(err.toString().contains('EMAIL_EXISTS')){
+      if (err.toString().contains('EMAIL_EXISTS')) {
         errorMessage = 'This email address is already in use';
-      } else if (err.toString().contains('INVALID_PASSWORD')){
+      } else if (err.toString().contains('INVALID_PASSWORD')) {
         errorMessage = 'Invalid password';
-      } else if (err.toString().contains('WEAK_PASSWORD')){
+      } else if (err.toString().contains('WEAK_PASSWORD')) {
         errorMessage = 'This password is too weak';
       }
       _showErrorDialog(errorMessage);
-    } catch(err){
+    } catch (err) {
       var errorMessage = 'Authentication failed, try again later';
       _showErrorDialog(errorMessage);
     }
-  
+
     setState(() {
       _isLoading = false;
     });
@@ -160,10 +189,12 @@ class _AuthCardState extends State<AuthCard> {
       setState(() {
         _authMode = AuthMode.Signup;
       });
+      _controller.forward();
     } else {
       setState(() {
         _authMode = AuthMode.Login;
       });
+      _controller.reverse();
     }
   }
 
@@ -176,9 +207,10 @@ class _AuthCardState extends State<AuthCard> {
       ),
       elevation: 8.0,
       child: Container(
-        height: _authMode == AuthMode.Signup ? 320 : 260,
+        //height: _authMode == AuthMode.Signup ? 320 : 260,
+        height: _heightAnimation.value.height,
         constraints:
-            BoxConstraints(minHeight: _authMode == AuthMode.Signup ? 320 : 260),
+            BoxConstraints(minHeight: _heightAnimation.value.height),
         width: deviceSize.width * 0.75,
         padding: EdgeInsets.all(16.0),
         child: Form(
